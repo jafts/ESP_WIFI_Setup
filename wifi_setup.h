29c00172a2520b24324978fc9487be3210133b7f
jafts <jfouts@gmail.com>
2023-12-24 16:24:30 -0800

    Update wifi_setup.h

--- wifi_setup.h
+++ wifi_setup.h
@@ -4,8 +4,14 @@
 #include <WiFi.h>
 #include <Preferences.h>
 
-extern char ssid[32];
-extern char password[64];
+// Define keys for storing credentials
+const char* const clientModeSSIDKey = "client_ssid";
+const char* const clientModePasswordKey = "client_password";
+const char* const apModeSSIDKey = "ap_ssid";
+const char* const apModePasswordKey = "ap_password";
+
+extern char ssid[32];    // Max SSID length is 32 bytes
+extern char password[64]; // Max WPA2 password length is 63 bytes
 extern bool isInAPMode;
 
 void setupWiFi(const char* baseSSID, const char* defaultPassword);
