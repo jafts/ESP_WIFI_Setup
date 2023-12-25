a546cba1120da4c7b524eec78310115afc7726b8
jafts <jfouts@gmail.com>
2023-12-24 16:21:09 -0800

    Update ESP_WIFI_CONFIG.ino

--- ESP_WIFI_CONFIG.ino
+++ ESP_WIFI_CONFIG.ino
@@ -1,15 +1,15 @@
 #include <Arduino.h>
+#include <SPIFFS.h>
 #include "wifi_setup.h"
 #include "custom_web.h"
 
-void setup() {
+void setup() {    
   Serial.begin(115200);
-  
   // Setup WiFi with a base SSID and a default password
-  const char* baseSSID = "ESP32-AP-";
+  const char* baseSSID = "LEDAP-";
   const char* defaultPassword = "123456789";
-
   setupWiFi(baseSSID, defaultPassword);
+
 }
 
 void loop() {
